13d3b4e4364baec5a7a06ada5953910bb3f4a5c8
JulienC-Dev <julien.cormier@yahoo.fr>
2021-05-28 10:30:10 +0200

    ajout recherche information product

--- bookscraper.py
+++ bookscraper.py
@@ -1,3 +1,21 @@
 import requests
-a = "test"
-print(a)
+from bs4 import BeautifulSoup
+
+url = "http://books.toscrape.com/catalogue/in-a-dark-dark-wood_963/index.html"
+
+res = requests.get(url)
+if res.ok:
+    soup = BeautifulSoup(res.text, 'lxml')
+    title = soup.find('title')
+    print(title.text)
+    table = soup.findAll('td')
+    tds = []
+    for i in table:
+        tds.append(i)
+    print(tds[0].get_text())
+    print(tds[3].get_text())
+    print(tds[2].get_text())
+
+
+
+
